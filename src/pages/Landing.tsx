6791eb66782a21339d3c2250dea0d913757e378b
Samuel Alegre <samuel.alegre.castro@gmail.com>
2023-07-17 14:55:09 +0200

    Solucionado scroll por Fran

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,15 +4,16 @@ import { Header } from "../components/moleculi/Header.component";
 import { Hero } from "../components/moleculi/Hero.component";
 import { Projects } from "../components/moleculi/Projects.component";
 
-
 const Landing = () => {
   return (
-    <div className="max-w-screen max-h-screen overflow-scroll">
+    <div className="max-w-screen max-h-screen overflow-hidden">
       <Header />
-      <Hero />
-      <Projects />
-      <Contact />
-      <Footer />
+      <div className="mt-20 max-h-screen overflow-scroll">
+        <Hero />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
     </div>
   );
 };
