01b198c104211014ccd55ad6dc8d49704afea790
Samuel Alegre <samuel.alegre.castro@gmail.com>
2023-07-19 12:07:04 +0200

    Updates

--- src/components/atoms/winnerWindow/WinnerWindow.component.tsx
+++
@@ -1,9 +0,0 @@
-export const WinnerWindow = () => {
-  return (
-   <>
-    <div className="flex justify-center items-center bg-white w-1/4 h-24 absolute rounded-lg shadow-xl">
-        <h1 className="">¡Enhorabuena X, eres el ganador!</h1>
-    </div>
-   </> 
-  )
-};
